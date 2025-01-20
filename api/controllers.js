944de695db242c5d96d43cd437d1a89706f78fbb
johanan-daniel <70776118+johanan-daniel@users.noreply.github.com>
2025-01-19 20:06:27 -0500

    fix

--- api/controllers.js
+++ api/controllers.js
@@ -449,7 +449,7 @@ const get_reddit_programmer_humor = async (req, res) => {
     // goes through Google translate because either Reddit or Node isn't playing nice
     const data = await (
         await fetch(
-            'https://www-reddit-com.translate.goog/r/ProgrammerHumor/top.json?t=today&limit=10&_x_tr_sl=fr&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp'
+            'https://www-reddit-com.translate.goog/r/ProgrammerHumor/top.json?t=today&limit=5&_x_tr_sl=fr&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp'
         )
     ).json()
 
@@ -462,8 +462,8 @@ const get_reddit_programmer_humor = async (req, res) => {
     items = items.filter((item) => item)
 
     const updatesObj = {
-        title: 'r/Purdue',
-        home_page_url: 'https://www.reddit.com/r/Purdue',
+        title: 'r/ProgrammerHumor',
+        home_page_url: 'https://www.reddit.com/r/ProgrammerHumor',
         feed_url: `${baseURL}${req.path}`,
         favicon:
             'https://www.redditstatic.com/shreddit/assets/favicon/64x64.png',
