dee09eb1da306ee23f0f9489aee0e95b98d642e0
johanan-daniel <70776118+johanan-daniel@users.noreply.github.com>
2025-10-01 12:23:15 -0400

    refactor backlon threads

--- api/controllers.js
+++ api/controllers.js
@@ -9,11 +9,12 @@ import {
     parseRedditFeedIntoItems,
     baseURL,
 } from './utils.js'
-import { xml2js } from 'xml-js'
+// import { xml2js } from 'xml-js'
 import { getJsonFeed as getNotionJsonFeed } from './services/notionService.js'
 import { getJsonFeed as getAvailableFeedsJsonFeed } from './services/availableFeedsService.js'
 import { getJsonFeed as getXkcdJsonFeed } from './services/xkcdService.js'
 import { getJsonFeed as getBbcTravelJsonFeed } from './services/bbcTravelService.js'
+import { getJsonFeed as getThreadsBacklonJsonFeed } from './services/threadsBacklonService.js'
 
 const getExampleXML = (req, res) => {
     logResponseDetails(req, res)
@@ -86,66 +87,13 @@ const getTimelessArticles = async (req, res) => {
 }
 
 const get_xkcd = async (req, res) => {
-    // if (!logRequestDetails(req, res, { checkForAnyParams: true })) return
-
-    // logResponseDetails(req, res)
     const jsonFeed = await getXkcdJsonFeed(req)
     return res.send(jsonFeed)
 }
 
 const get_backlon_threads = async (req, res) => {
-    let data = []
-    await fetch(
-        'https://mastodon.world/api/v1/accounts/112136961701411930/statuses?exclude_replies=true'
-    )
-        .then((res) => {
-            return res.json()
-        })
-        .then((items) => (data = items))
-
-    const items = data.map((item) => {
-        let content = item['content']
-        let regex = '<.*?>'
-        var re = new RegExp(regex, 'g')
-        content = content.replace(re, '')
-
-        // let image = item['media_attachments']['preview_url']
-        let html = ''
-        if (item['media_attachments'].length > 0) {
-            const img_url = item['media_attachments'][0]['preview_url']
-            html = `<img src=${img_url} />`
-        }
-
-        // console.log(html)
-
-        const object = {
-            title: content,
-            url: item['url'],
-            id: item['url'],
-            summary: content,
-            date_published: item['created_at'],
-            content_html: `<p>${item['content']}</p>${html}`,
-            // image: item['media_attachments']['preview_url'],
-        }
-
-        return object
-    })
-
-    const account_info = data[0]['account']
-    const updatesObj = {
-        title: 'Dieter Bohn',
-        home_page_url: account_info['url'],
-        feed_url: `${baseURL}${req.route.path}`,
-        authors: [{ name: account_info['display_name'] }],
-        description:
-            'Working at Google, formerly founded Verge, Android Central, iMore, Windows Central, PreCentral. Enjoyer of puns, protected bike lanes, and typos',
-        icon: account_info['avatar'],
-        items,
-    }
-
-    const json = updateJSONWithObject(updatesObj)
-
-    res.send(json)
+    const jsonFeed = await getThreadsBacklonJsonFeed(req)
+    res.send(jsonFeed)
 }
 
 const get_bing_image = async (req, res) => {
