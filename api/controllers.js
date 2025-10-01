a9a72b1287ff2844400b4ab5790c417b92fc0d45
johanan-daniel <70776118+johanan-daniel@users.noreply.github.com>
2025-10-01 14:34:30 -0400

    refactor reddit purdue

--- api/controllers.js
+++ api/controllers.js
@@ -15,6 +15,7 @@ import { getJsonFeed as getXkcdJsonFeed } from './services/xkcdService.js'
 import { getJsonFeed as getBbcTravelJsonFeed } from './services/bbcTravelService.js'
 import { getJsonFeed as getThreadsBacklonJsonFeed } from './services/threadsBacklonService.js'
 import { getJsonFeed as getBingImageJsonFeed } from './services/bingImageService.js'
+import { getJsonItems as getRedditPurdueJsonFeed } from './services/redditPurdueService.js'
 
 const getExampleXML = (req, res) => {
     logResponseDetails(req, res)
@@ -170,33 +171,8 @@ const get_tom_scott = async (req, res) => {
 }
 
 const get_reddit_purdue = async (req, res) => {
-    // goes through Google translate because either Reddit or Node isn't playing nice
-    const data = await (
-        await fetch(
-            'https://www-reddit-com.translate.goog/r/Purdue/top.json?t=today&limit=10&_x_tr_sl=fr&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp'
-        )
-    ).json()
-
-    const raw_items = data['data']['children']
-
-    // Additional HTTP requests get made, so this is wrapped in a Promise.all
-    let items = await parseRedditFeedIntoItems(raw_items, 100, 35, 40)
-
-    // Removes null items that were skipped in map
-    items = items.filter((item) => item)
-
-    const updatesObj = {
-        title: 'r/Purdue',
-        home_page_url: 'https://www.reddit.com/r/Purdue',
-        feed_url: `${baseURL}${req.path}`,
-        favicon:
-            'https://www.redditstatic.com/shreddit/assets/favicon/64x64.png',
-        items,
-    }
-
-    const json = updateJSONWithObject(updatesObj)
-
-    res.send(json)
+    const jsonFeed = await getRedditPurdueJsonFeed(req.path)
+    res.send(jsonFeed)
 }
 
 const get_reddit_programmer_humor = async (req, res) => {
